1c724b6d4c97be27d31888c622ff795985edda3d
SaidBC <razzouksaid139@gmail.com>
2024-09-24 16:05:48 +0100

    test db

--- db/pool.js
+++ db/pool.js
@@ -3,5 +3,5 @@ const main = require('./populate');
 
 main()
 module.exports = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: 'postgres://default:eXc2dogzTH4v@ep-green-bush-a4o8psv6.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require'
 })
